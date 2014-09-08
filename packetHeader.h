3cdf3e4b337096d84d54e9a1688522bd50947745
Kaushik N <kaushik_38@yahoo.co.in>
2014-09-08 15:33:46 -0700

    Added ACK

--- packetHeader.h
+++ packetHeader.h
@@ -1,13 +1,16 @@
 #ifndef PACKET_H
 #define PACKET_H
-
 #include <stdio.h>
-
+#define SEQ_PACK 350
 typedef struct 
 {
 	int sequenceNo;
 	int len;
 	char data[1400];
 } customPacket;
-
+typedef struct
+{
+	int length;
+	int seqNo[SEQ_PACK];
+} seqACK;
 #endif
