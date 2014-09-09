b7320c284473c2cc275c057fb0416d188054350f
Kapil Dev <kapil.dev87@gmail.com>
2014-09-09 01:30:22 -0700

    updated with new nackfile header
    To get the file size(>0) and nack(==-1)

--- packetHeader.h
+++ packetHeader.h
@@ -4,14 +4,23 @@
 #define SEQ_PACK 350
 typedef struct 
 {
-	char type;
 	int sequenceNo;
 	int len;
 	char data[1400];
 } customPacket;
+
+//from server to client
 typedef struct
 {
+
 	int length;
 	int seqNo[SEQ_PACK];
 } seqACK;
+
+typedef struct 
+{
+	int data;
+	
+} nackFile;
+
 #endif
